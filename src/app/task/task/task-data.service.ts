abb4c8c36c9c5001d804819871267edca29a3393
Lennert Hofman <lennerthofman@hotmail.com>
2017-12-03 15:31:57 +0100

    debugging

--- src/app/task/task/task-data.service.ts
+++ src/app/task/task/task-data.service.ts
@@ -33,11 +33,8 @@ export class TaskDataService {
 
   findTasksOnDate(date1: Date, date2?: Date): Observable<Task[]> {
     return this.http.get(this._appUrl + 'tasks/' + date1 + (date2 ? ('/' + date2) : '')).map(response =>
-      response.json().map(item => {
-        console.log(item);
-        return Task.fromJSON(item);
-      }
-        ));
+      response.json().map(item =>
+        Task.fromJSON(item)));
   }
 
   removeTask(id): Observable<String> {
